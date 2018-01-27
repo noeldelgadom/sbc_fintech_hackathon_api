574f2f4ed501c1ea83ef35159364792ba4160fad
Noel Delgado <noeldelgadom@gmail.com>
2018-01-27 11:32:46 -0600

    account balance, account statment and transfer works

--- lib/fiinlab_tool.rb
+++ lib/fiinlab_tool.rb
@@ -25,9 +25,63 @@ module FiinlabTool
         :x_access_token => session['token'],
         :content_type => 'application/json'
       login_response = JSON.parse(response.body).with_indifferent_access
-      return login_response['data'] ? login_response['data'] : login_response['message']
+      login_response['data'] ? session : login_response
     else
-      return session['messge']
+      session['messge']
+    end
+  end
+
+  def self.account_balance
+    session = FiinlabTool.login
+    if session['token']
+      response = RestClient.post '217.32.246.192:9091/ESBConnector/esb/core/transaction',
+        {
+          "trans_type": "BENQ0002",
+          "account": "01",
+          "custaccount": "113183314"
+        }.to_json,
+        :x_access_token => session['token'],
+        :content_type => 'application/json'
+      balance_response = JSON.parse(response.body).with_indifferent_access
+    else
+      session['messge']
+    end
+  end
+
+  def self.account_statement
+    session = FiinlabTool.login
+    if session['token']
+      response = RestClient.post '217.32.246.192:9091/ESBConnector/esb/core/transaction',
+        {
+          "trans_type": "STMNT0002",
+          "account": "01",
+          "custaccount": "113183314"
+        }.to_json,
+        :x_access_token => session['token'],
+        :content_type => 'application/json'
+      statement_response = JSON.parse(response.body).with_indifferent_access
+    else
+      session['messge']
+    end
+  end
+
+  def self.transfer(from_account, to_account, amount, reference)
+    session = FiinlabTool.login
+    if session['token']
+      response = RestClient.post '217.32.246.192:9091/ESBConnector/esb/core/transaction',
+        {
+          "trans_type": "INTTFR0002",
+          "account": "00",
+          "fromaccountnumber": from_account,
+          "toaccountnumber": to_account,
+          "amount": amount,
+          "reference": reference
+        }.to_json,
+        :x_access_token => session['token'],
+        :content_type => 'application/json'
+      transfer_response = JSON.parse(response.body).with_indifferent_access
+    else
+      session['messge']
     end
   end
 end
