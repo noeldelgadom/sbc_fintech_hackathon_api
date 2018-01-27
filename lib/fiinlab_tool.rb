9a0a41222068ee652ee86272254c0911c5fdf391
Noel Delgado <noeldelgadom@gmail.com>
2018-01-27 10:33:36 -0600

    create session works

--- lib/fiinlab_tool.rb
+++ lib/fiinlab_tool.rb
@@ -2,8 +2,14 @@ module FiinlabTool
   require 'rest-client'
   require 'json'
 
-  def self.retrieve_data(url)
-    response  = RestClient.get URI::encode(url)
+  def self.create_session
+    response = RestClient.post '217.32.246.192:9091/ESBConnector/createSession',
+      {
+        "organizationID": "COMPARTAMOS",
+        "channel": "WEBSERVICE",
+        "application": "CMPXA003",
+        "username": "TCPIP"
+      }.to_json, :content_type => 'application/json'
     JSON.parse(response.body).with_indifferent_access
   end
 end
