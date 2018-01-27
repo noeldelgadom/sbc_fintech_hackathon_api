b5c5986a463ff113cdbf2daeb77d5d7a823d6c70
Noel Delgado <noeldelgadom@gmail.com>
2018-01-27 12:54:33 -0600

    tranfser works

--- app/controllers/transfers_controller.rb
+++ app/controllers/transfers_controller.rb
@@ -1,6 +1,12 @@
 class TransfersController < ApplicationController
   def create
-    @body = JSON.parse(request.body.read)
-    puts @body
+    body = JSON.parse(request.body.read)
+
+    from_account  = User.find(body['from_id']).account_number     if body['from_model'] == 'User'
+    from_account  = Company.find(body['from_id']).account_number  if body['from_model'] == 'Company'
+    to_account    = User.find(body['to_id']).account_number       if body['to_model'] == 'User'
+    to_account    = Company.find(body['to_id']).account_number    if body['to_model'] == 'Company'
+
+    FiinlabTool.transfer(from_account, to_account, body['ammount'], body['reference'])
   end
 end
